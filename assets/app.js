d02c105ff79cc1106daef0a41ec2b43881bd82f7
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-20 16:08:06 +0200

    add email on page

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        emailList: []
       }
+    },
+    
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.emailList.push(response.data.response)
+            })
+            
+        }
     }
+        
+    
   }).mount('#app')
